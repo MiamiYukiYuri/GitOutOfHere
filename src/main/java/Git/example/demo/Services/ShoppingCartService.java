5b80648ddaf3785679b32a907e97b1b0bc6221e3
Mia Stefansdotter <stefansdotter@gmail.com>
2024-02-06 10:20:21 +0100

    working on shopping cart service

--- src/main/java/Git/example/demo/Services/ShoppingCartService.java
+++ src/main/java/Git/example/demo/Services/ShoppingCartService.java
@@ -1,4 +1,22 @@
 package Git.example.demo.Services;
 
+import Git.example.demo.Repository.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ShoppingCartService {
+    @Autowired
+    GameRepository gameRepository;
+
+
+    // CREATE new shopping cart
+
+
+    // ADD game to shopping cart
+
+
+
+    // DELETE game from shopping cart
+
 }
